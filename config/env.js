ee8a92a1fa17a1c486fbf4f03ca24f9a9aafa0c8
Sandro <sestrada@acid.cl>
2021-11-05 10:49:11 -0600

    feat(env): update env variables

--- config/env.js
+++ config/env.js
@@ -6,9 +6,12 @@ module.exports = {
   development: {
     mercadoPago:
       "TEST-2728193141713947-102913-e26d87d9f8251c0b64a1e794e340e3d6-345192359",
-    IMAGEKIT_URL_ENDPOINT: "",
-    IMAGEKIT_PUBLIC_KEY: "",
-    IMAGEKIT_PRIVATE_KEY: "",
+    IMAGEKIT_URL_ENDPOINT: "https://ik.imagekit.io/kmujnmitqkk",
+    IMAGEKIT_PUBLIC_KEY: "public_IH8hHG5eKEegA2Dr4UP5XbuPP6Y=",
+    IMAGEKIT_PRIVATE_KEY: "private_MzJWYkGIT7ZjwgBxrUiO2JJsEOM=",
+    successUrl: "http://localhost:8080/feedback",
+    failureUrl: "http://localhost:8080/feedback",
+    pendingUrl: "http://localhost:8080/feedback",
   },
   production: {
     mercadoPago:
@@ -16,5 +19,8 @@ module.exports = {
     IMAGEKIT_URL_ENDPOINT: "",
     IMAGEKIT_PUBLIC_KEY: "",
     IMAGEKIT_PRIVATE_KEY: "",
+    successUrl: "http://localhost:8080/feedback",
+    failureUrl: "http://localhost:8080/feedback",
+    pendingUrl: "http://localhost:8080/feedback",
   },
 };
